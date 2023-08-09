3e1fea22ae64729a8554c3829936f02f6263c34d
srudraDev <suharta098@gmail.com>
2023-08-09 12:06:47 +0000

    push submitted applications to database

--- database.py
+++ database.py
@@ -25,5 +25,18 @@ def load_job_from_db(id):
       return None
     else:
       return rows[0]._asdict()
-    
+
+def add_application_to_db(job_id,data,resume):
+  parameters = {
+    "job_id": job_id,
+    "full_name": data['full_name'],
+    "email": data['email'],
+    "LinkedIn": data['LinkedIn'],
+    "education": data['education'],
+    "work_experience": data['work_experience'],
+    "resume": resume
+  }
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, LinkedIn, education, work_experience, resume) VALUES (:job_id, :full_name, :email, :LinkedIn, :education, :work_experience, :resume)")
+    conn.execute(query,parameters)
 
